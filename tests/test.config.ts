2265777d25a068093369985013094cae561d306e
TomFinchCA <tomfinchcdd@gmail.com>
2017-07-18 16:27:43 -0500

    Update test.config.ts

--- tests/test.config.ts
+++ tests/test.config.ts
@@ -1,3 +1,3 @@
 
 // export const TEST_URL: string = "file:///D:/workspace/nolio/JavaScript-Sample-Application/index.html";
-export const TEST_URL: string = "http://localhost:9000/";
+export const TEST_URL: string = "ec2-13-58-236-223.us-east-2.compute.amazonaws.com";
