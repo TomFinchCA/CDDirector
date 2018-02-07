e186d315ec814a72e09a0b048ecff30cf03f3294
TomFinchCA <tomfinchcdd@gmail.com>
2018-02-07 10:18:44 +0000

    Create todo-spec.js

---
+++ tests/todo-spec.js
@@ -0,0 +1,7 @@
+describe('Tom Demo Functional', function() {
+  it('click icon', function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://tomsdemoapp.us-east-2.elasticbeanstalk.com/');
+    element(by.linkText('here')).click();
+    });
+});
